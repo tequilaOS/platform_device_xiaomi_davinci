ed95c2b717cc455dc032de5be9ed0a63a4241f7e
TheStrechh <TheStrechh@gmail.com>
2022-12-10 23:58:59 +0000

    davinci: set patch kernel source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_CONFIG := davinci_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
+TARGET_KERNEL_SOURCE := kernel/xiaomi/davinci
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := yuki
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/yuki-clang
