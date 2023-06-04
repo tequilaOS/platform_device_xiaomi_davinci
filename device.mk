d308158abcb2763f1244b0dced78e1f4c41398cd
Arian <arian.kulmer@web.de>
2023-06-04 16:11:37 +0200

    davinci: Switch to dynamic partitions
    Change-Id: I49cd2f672e4d6feab5f21743186c62639157d3eb

--- device.mk
+++ device.mk
@@ -88,6 +88,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-tequila
 
+# Partitions
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 # Power
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
